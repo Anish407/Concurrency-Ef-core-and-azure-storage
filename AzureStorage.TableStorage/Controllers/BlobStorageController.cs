01fee7ff28b622fae1b1632bac2039334d9b74e5
Anish Aravind <anish.arvind407@gmail.com>
2020-02-06 00:47:16 +0530

    chnages

---
+++ AzureStorage.TableStorage/Controllers/BlobStorageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace TableStorage.CRUD.Controllers
+{
+    [Route("BlobStorage")]
+    public class BlobStorageController : Controller
+    {
+        public BlobStorageController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        [HttpPost("CreateContainer")]
+        public async Task<IActionResult> CreateContainer()
+        {
+            string constring = this.Configuration["connection"];
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(constring);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("Images");
+            await container.CreateIfNotExistsAsync();
+
+            return Ok();
+        }
+    }
+}
